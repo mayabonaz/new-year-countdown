e20e90343e40b182a15ddc8cd641db4f3b96e894
Muhayyo <muhayyo@hotmail.com>
2020-06-04 22:06:02 +0100

    finished countdown timer

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+// Constants and variables
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+let countDownDate = new Date('Dec 31, 2020 00:00:00').getTime(),
+    x = setInterval(function() {
+      let currentTime = new Date().getTime(),
+      distance = countDownDate - currentTime;
+
+      document.querySelector('#days').textContent = Math.floor(distance / day),
+      document.querySelector('#hours').innerText = Math.floor(((distance % day)) / hour),
+      document.querySelector('#minutes').innerText = Math.floor((distance % hour) / minute),
+      document.querySelector('#seconds').innerText = Math.floor((distance % minute) / second);
+
+    }, second)
